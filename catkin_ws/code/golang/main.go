dfbe12d4d09e0a243cc3c0cb91ebcecd294f4704
jkim <joekim29@gmail.com>
2020-05-03 17:36:53 -0400

    test goroslib implementation

---
+++ catkin_ws/code/golang/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aler9/goroslib"
+	"github.com/aler9/goroslib/msgs/sensor_msgs"
+)
+
+func onMessage(msg *sensor_msgs.Imu) {
+	fmt.Printf("Incoming: %+v\n", msg)
+}
+
+func main() {
+	// create a node with given name and linked to given master.
+	// master can be reached with an ip or hostname.
+	n, err := goroslib.NewNode(goroslib.NodeConf{
+		Name:       "/goroslib",
+		MasterHost: "donkeycar",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Connected to Master")
+	defer n.Close()
+
+	// create a subscriber
+	sub, err := goroslib.NewSubscriber(goroslib.SubscriberConf{
+		Node:     n,
+		Topic:    "/joy",
+		Callback: onMessage,
+		msgType: "joy",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Connected to topic")
+	defer sub.Close()
+
+	// freeze main loop
+	infty := make(chan int)
+	<-infty
+}
