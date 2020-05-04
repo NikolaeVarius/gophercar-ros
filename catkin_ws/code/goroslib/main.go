faa9e188cb557c2cc1e59f7967f19f8608d33bba
jkim <joekim29@gmail.com>
2020-05-04 19:25:44 -0400

    decompose Twist

--- catkin_ws/code/goroslib/main.go
+++ catkin_ws/code/goroslib/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/aler9/goroslib"
-	//"github.com/aler9/goroslib/msgs"
-        "github.com/aler9/goroslib/msgs/sensor_msgs"
+	"github.com/aler9/goroslib/msgs"
+	"github.com/aler9/goroslib/msgs/geometry_msgs"
+	"github.com/aler9/goroslib/msgs/sensor_msgs"
 )
+
+//type JoyEvent
 
 func onMessage(msg *sensor_msgs.Joy) {
 	fmt.Printf("Incoming: %+v\n", msg)
+	x_float64 := msgs.Float64(float64(msg.Axes[0]))
+	y_float64 := msgs.Float64(float64(msg.Axes[1]))
+	linearVector := geometry_msgs.Vector3{X: x_float64, Y: y_float64}
+	rawMove := geometry_msgs.Twist{Linear: linearVector, Angular: linearVector}
+	stampedMove := geometry_msgs.TwistStamped{Header: msg.Header, Twist: rawMove}
+	fmt.Printf("Outgoing: %+v\n", stampedMove)
+	//fmt.Printf(msg.Header)
+	//fmt.Printf(msg.Axes)
+	//fmt.Printf(msg.Buttons)
 }
 
 func main() {
