fb2741a3671e27ec52fc5ecf78dea1988db9ee71
jkim <joekim29@gmail.com>
2020-05-05 18:23:14 -0400

    that could have ended badly

--- catkin_ws/code/drive-node/main.go
+++ catkin_ws/code/drive-node/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"log"
-
 	"periph.io/x/periph/conn/i2c/i2creg"
 	"periph.io/x/periph/experimental/devices/pca9685"
 	"periph.io/x/periph/host"
+	//	"time"
 )
 
 func main() {
@@ -24,10 +24,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := pca.SetPwmFreq(100); err != nil {
+	//if err := pca.SetPwmFreq(300); err != nil {
+	//		log.Fatal(err)
+	//	}
+
+	// 400 turns on gogo juice. this is dangerous when on a makeshift test stand
+	if err := pca.SetAllPwm(400, 600); err != nil {
 		log.Fatal(err)
 	}
-	if err := pca.SetAllPwm(200, 300); err != nil {
-		log.Fatal(err)
-	}
+	//	time.Sleep(2 * time.Second)
+	//	if err := pca.SetAllPwm(500, 500); err != nil {
+	//		log.Fatal(err)
+	//	}
+
 }
