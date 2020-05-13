5e0f29c1f9da5d97a81fa14d0aa761a45660e21b
jkim <joekim29@gmail.com>
2020-05-12 22:43:12 -0400

    cleanup

--- catkin_ws/code/joy-goroslib/main.go
+++ catkin_ws/code/joy-goroslib/main.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"fmt"
-//	"time"
 	"github.com/aler9/goroslib"
 	"github.com/aler9/goroslib/msgs"
 	"github.com/aler9/goroslib/msgs/geometry_msgs"
 	"github.com/aler9/goroslib/msgs/sensor_msgs"
-//        "gopkg.in/alexcesaro/statsd.v2"
+	//        "gopkg.in/alexcesaro/statsd.v2"
 )
 
 var subTopic *goroslib.Subscriber
 var pubTopic *goroslib.Publisher
 
-//type Message struct {
-//	msgs.Package `ros:"my_package"`
-//	LinearX   msgs.float64
-//	LinearY   msgs.float64
-//}
-
-func covertJoyToTwistStamped(msg *sensor_msgs.Joy) geometry_msgs.TwistStamped  {
+func covertJoyToTwistStamped(msg *sensor_msgs.Joy) geometry_msgs.TwistStamped {
 	fmt.Println("Incoming: %+v\n", msg)
 	x_float64 := msgs.Float64(float64(msg.Axes[0]))
 	y_float64 := msgs.Float64(float64(msg.Axes[1]))
@@ -30,8 +23,6 @@ func covertJoyToTwistStamped(msg *sensor_msgs.Joy) geometry_msgs.TwistStamped  {
 }
 
 func main() {
-
-	//stampedTwistMessages := make(chan *geometry_msgs.TwistStamped, 100)
 	joyMessages := make(chan *sensor_msgs.Joy, 100)
 
 	n, err := goroslib.NewNode(goroslib.NodeConf{
@@ -43,14 +34,12 @@ func main() {
 	}
 	fmt.Println("Connected to Master")
 	defer n.Close()
-    
-	// create a subscriber
+
 	subTopic, err = goroslib.NewSubscriber(goroslib.SubscriberConf{
-		Node:     n,
-		Topic:    "/joy",
+		Node:  n,
+		Topic: "/joy",
 		Callback: func(msg *sensor_msgs.Joy) {
-	          //fmt.Println("here")
-		  joyMessages <- msg
+			joyMessages <- msg
 		},
 	})
 	if err != nil {
@@ -71,20 +60,18 @@ func main() {
 	}
 	fmt.Println("Connected to Publisher Topic")
 	defer pubTopic.Close()
-	
-    go func() {
-        for x := range joyMessages  {
-            publishMessage(covertJoyToTwistStamped(x))
-        }
-    }()
+
+	go func() {
+		for x := range joyMessages {
+			publishMessage(covertJoyToTwistStamped(x))
+		}
+	}()
 
 	infty := make(chan int)
 	<-infty
 }
 
 func publishMessage(msg geometry_msgs.TwistStamped) {
-    fmt.Println("Publishing")
-    pubTopic.Write(&msg)
-    fmt.Println("Done")
-    return
+	pubTopic.Write(&msg)
+	return
 }
