1549f3927c49b3c49a3a9a2471d6ef730d900e13
jkim <joekim29@gmail.com>
2020-05-16 15:55:16 -0400

    test out setting pwm values per tick

--- catkin_ws/code/drive-node/main.go
+++ catkin_ws/code/drive-node/main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"log"
 	"fmt"
+	"github.com/aler9/goroslib"
+	"github.com/aler9/goroslib/msgs"
+	"github.com/aler9/goroslib/msgs/geometry_msgs"
+	"log"
+	"periph.io/x/periph/conn/gpio"
 	"periph.io/x/periph/conn/i2c/i2creg"
 	"periph.io/x/periph/experimental/devices/pca9685"
 	"periph.io/x/periph/host"
-	"periph.io/x/periph/conn/gpio"
-        "github.com/aler9/goroslib"
-        "github.com/aler9/goroslib/msgs"
-        "github.com/aler9/goroslib/msgs/geometry_msgs"
-       // "github.com/aler9/goroslib/msgs/sensor_msgs"
-
-	//	"time"
+	// "github.com/aler9/goroslib/msgs/sensor_msgs"
+	"time"
 )
 
 // Steering Angle Parameters
@@ -22,7 +21,6 @@ const MAX_LEFT_PULSE = 400
 const MAX_RIGHT_PULSE = 200
 const NEUTRAL_PULSE = 300
 
-
 // Throttle Paramters
 const STOP_PULSE = 0
 const MIN_THROTTLE = 0 // maybe should just use STOP_PULSE?
@@ -31,6 +29,7 @@ const MIN_THROTTLE_PULSE = 0
 const MAX_THROTTLE_PULSE = 0
 const THROTTLE_CHANNEL = 0
 const THROTTLE_STEP = 10
+
 var pca *pca9685.Dev
 
 func main() {
@@ -50,21 +49,21 @@ func main() {
 	}
 
 	// PWM Freq
-        //if err := pca.SetPwmFreq(60); err != nil {
-       //       log.Fatal(err)
-        //}
-	
-        // Channel 0 = Throttle
+	//if err := pca.SetPwmFreq(60); err != nil {
+	//       log.Fatal(err)
+	//}
+
+	// Channel 0 = Throttle
 	// Initialize PWM throttle at a STOPPED Value
-        if err := pca.SetPwm(0, 0, STOP_PULSE); err != nil {
-              log.Fatal(err)
-        }
+	if err := pca.SetPwm(0, 0, STOP_PULSE); err != nil {
+		log.Fatal(err)
+	}
 
 	// Channel 1 = Steering
 	// Initialize PWM Steering at a Neutral Value
-        if err := pca.SetPwm(1, 0, NEUTRAL_PULSE); err != nil {
-              log.Fatal(err)
-        }
+	if err := pca.SetPwm(1, 0, NEUTRAL_PULSE); err != nil {
+		log.Fatal(err)
+	}
 
 	// https://github.com/google/periph/blob/master/conn/gpio/gpio.go read this
 	// 400 turns on gogo juice. this is dangerous when on a makeshift test stand
@@ -73,98 +72,117 @@ func main() {
 	//if err := pca.SetPwm(0, 0, 300); err != nil {
 	//        log.Fatal(err)
 	//}
-        actuatorMessages := make(chan *geometry_msgs.TwistStamped, 100)
+	actuatorMessages := make(chan *geometry_msgs.TwistStamped, 100)
 
-        n, err := goroslib.NewNode(goroslib.NodeConf{
-                Name:       "/gophercar-actuator",
-                MasterHost: "donkeycar",
-        })
-        if err != nil {
-                panic(err)
-        }
-        fmt.Print("Connected to Master")
-        defer n.Close()
-
-        // create a subscriber
-        subTopic, err := goroslib.NewSubscriber(goroslib.SubscriberConf{
-                Node:     n,
-                Topic:    "/actuator",
-                Callback: func(msg  *geometry_msgs.TwistStamped) {
-                        actuatorMessages <- msg
-                },
+	n, err := goroslib.NewNode(goroslib.NodeConf{
+		Name:       "/gophercar-actuator",
+		MasterHost: "donkeycar",
 	})
-        if err != nil {
-                panic(err)
-        }
-        fmt.Println("Connected to subscriber topic")
-        defer subTopic.Close()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Connected to Master")
+	defer n.Close()
 
-        go func() {
-                for x := range actuatorMessages {
-		  fmt.Println(x.Header)
-		  fmt.Println(x.Twist.Linear.X)
-		  err := setSteering(x.Twist.Linear.X)
-		  if err != nil {
-		    panic(err)
-		  }
-		  //fmt.Println(x.Twist.Linear.Y)
-		  //fmt.Println(x.Twist.Linear.Z)
-		  //fmt.Println(x.Twist.Angular)
-                }
+	// create a subscriber
+	subTopic, err := goroslib.NewSubscriber(goroslib.SubscriberConf{
+		Node:  n,
+		Topic: "/actuator",
+		Callback: func(msg *geometry_msgs.TwistStamped) {
+			actuatorMessages <- msg
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to subscriber topic")
+	defer subTopic.Close()
 
-        }()
+	ticker := time.NewTicker(100)
+	go func() {
+		for x := range actuatorMessages {
+			select {
+			case <-ticker.C:
+				fmt.Println(x.Header)
+				fmt.Println(x.Twist.Linear.X)
+				err := setSteering(x.Twist.Linear.X)
+				if err != nil {
+					panic(err)
+				}
+				//fmt.Println(x.Twist.Linear.Y)
+				//fmt.Println(x.Twist.Linear.Z)
+				//fmt.Println(x.Twist.Angular)
+			}
+		}
 
-        infty := make(chan int)
-        <-infty
+	}()
+
+	infty := make(chan int)
+	<-infty
 }
 
 func convertStampedTwistedToAngle() {
-    return
+	return
 }
-
 
 // Translate from input to throttle control pwm values
 func setThrottle(throttle msgs.Float64) error {
-    throttlePWMVal := getThrottlePWMVal(throttle)
-    fmt.Println(throttlePWMVal)
-    return nil
+	throttlePWMVal := getThrottlePWMVal(throttle)
+	fmt.Println(throttlePWMVal)
+	return nil
 }
 
 // Translate from intput to direction pwm values
 func setSteering(steering msgs.Float64) error {
-    val := getSteeringPWMVal(steering)
-    fmt.Println(val)
-    steeringPWMVal,dutyErr := gpio.ParseDuty("33%")
-    if dutyErr != nil {
-      return dutyErr
-    }
+	val := getSteeringPWMVal(steering)
+	fmt.Println(val)
+	steeringPWMVal, dutyErr := gpio.ParseDuty("33%")
+	if dutyErr != nil {
+		return dutyErr
+	}
 
-    if err := pca.SetPwm(1, 0, steeringPWMVal); err != nil {
-      return err
-    }
-    return nil
+	if err := pca.SetPwm(1, 0, steeringPWMVal); err != nil {
+		return err
+	}
+	return nil
 }
 
-func getThrottlePWMVal(val msgs.Float64) int{
-    return 0
+func getThrottlePWMVal(val msgs.Float64) int {
+	return 0
 }
 
 func getSteeringPWMVal(val msgs.Float64) int {
-  var pwmVal int
-  if (val < 0) {
-    fmt.Println("go right")
-    pwmVal = MAX_RIGHT_PULSE
-  } 
-  
-  if (val > 0) {
-    fmt.Println("go left")
-    pwmVal = MAX_LEFT_PULSE
-  }
+	var pwmVal int
+	if val < 0 {
+		fmt.Println("go right")
+		pwmVal = MAX_RIGHT_PULSE
+	}
 
-  if (val == 0) {
-    fmt.Println("stay straight")
-    pwmVal = NEUTRAL_PULSE
-  }
+	if val > 0 {
+		fmt.Println("go left")
+		pwmVal = MAX_LEFT_PULSE
+	}
 
-  return pwmVal
+	if val == 0 {
+		fmt.Println("stay straight")
+		pwmVal = NEUTRAL_PULSE
+	}
+
+	return pwmVal
+}
+
+// copy pasta to try it out some time in the future
+func Every(t time.Duration, f func()) *time.Ticker {
+	ticker := time.NewTicker(t)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				f()
+			}
+		}
+	}()
+
+	return ticker
 }
