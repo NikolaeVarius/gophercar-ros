8e6652cfba86e5e286a16fec4cca950a9c380b88
jkim <joekim29@gmail.com>
2020-05-17 19:40:56 -0400

    trying to hook up camera for opencv

--- catkin_ws/code/csi_camera/main.go
+++ catkin_ws/code/csi_camera/main.go
@@ -1,49 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"github.com/notedit/gst"
+	"gocv.io/x/gocv"
 )
 
 func main() {
-
-
-	pipelineStr := "nvarguscamerasrc sensor_id=0 ! nvvidconv flip-method=2 ! nvoverlaysink"
-	//pipelineStr = fmt.Sprintf(pipelineStr, 100)
-
-	err := gst.CheckPlugins([]string{"x264","rtp","videoparsersbad"})
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	pipeline, err := gst.ParseLaunch(pipelineStr)
-
-	if err != nil {
-		panic(err)
-	}
-
-	element := pipeline.GetByName("appsink")
-
-	pipeline.SetState(gst.StatePlaying)
+	webcam, _ := gocv.OpenVideoCapture(0)
+	window := gocv.NewWindow("Hello")
+	img := gocv.NewMat()
 
 	for {
-
-		sample, err := element.PullSample()
-		if err != nil {
-			if element.IsEOS() == true {
-				fmt.Println("eos")
-				return
-			} else {
-				fmt.Println(err)
-				continue
-			}
-		}
-		fmt.Println("got sample", sample.Duration)
+		webcam.Read(&img)
+		window.IMShow(img)
+		window.WaitKey(1)
 	}
-
-	pipeline.SetState(gst.StateNull)
-
-	pipeline = nil
-	element = nil
 }
