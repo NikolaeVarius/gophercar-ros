03f7af2bfb1195b9f6516f7ede6e18fa9a2d772d
jkim <joekim29@gmail.com>
2020-05-19 06:58:10 -0400

    testing out camera

--- catkin_ws/code/csi_camera/main.py
+++ catkin_ws/code/csi_camera/main.py
@@ -8,7 +8,7 @@ import cv2
 import sys, time
 import roslib
 import rospy
-from from cv_bridge import CvBridge
+from cv_bridge import CvBridge
 import numpy as np
 from scipy.ndimage import filters
 
@@ -48,10 +48,10 @@ class image_publisher:
         #self.subscriber.unregister()
 
 def gstreamer_pipeline(
-    capture_width=640,
-    capture_height=480,
-    display_width=640,
-    display_height=480,
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
     framerate=30,
     flip_method=2,
 ):
@@ -75,17 +75,22 @@ def gstreamer_pipeline(
     )
 
 def main(args):
-    rospy.init_node('image_feature', anonymous="false")
+    rospy.init_node('image_capture', anonymous="false")
     image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage)
-    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    print(gstreamer_pipeline(flip_method=2))
+
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
 
     if cap.isOpened():
         window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
-        # Window
         while cv2.getWindowProperty("CSI Camera", 0) >= 0:
             ret_val, img = cap.read()
             cv2.imshow("CSI Camera", img)
+            keyCode = cv2.waitKey(30) & 0xFF
+            if keyCode == 27:
+                break
         cap.release()
+        cv2.destroyAllWindows()
     else:
         print("Unable to open camera")
 
@@ -93,7 +98,6 @@ def main(args):
         rospy.spin()
     except KeyboardInterrupt:
         print( "Shutting down ROS Camera Publisher")
-    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
