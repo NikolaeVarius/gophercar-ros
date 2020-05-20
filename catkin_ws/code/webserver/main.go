494ab3e55a64f10bf9d60bf8fd7c6434fa90707c
jkim <joekim29@gmail.com>
2020-05-19 21:46:28 -0400

    trying stuff out

--- catkin_ws/code/webserver/main.go
+++ catkin_ws/code/webserver/main.go
@@ -89,7 +89,7 @@ func mjpegCapture(ch chan *sensor_msgs.CompressedImage) {
 	// ticker := time.NewTicker(100)
 	for x := range ch {
 
-		buf, _ := gocv.IMDecode(".jpg", x.Data)
+		buf, _ := gocv.IMDecode(x.Data)
 		stream.UpdateJPEG(buf)
 	}
 }
