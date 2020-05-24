b79e2bb3df6af5512767147189f2e424b0cb628a
jkim <joekim29@gmail.com>
2020-05-24 07:45:55 -0400

    approximate time sync, not sure if this is a good idea

--- catkin_ws/code/steer-sync-node/main.py
+++ catkin_ws/code/steer-sync-node/main.py
@@ -3,20 +3,32 @@
 
 import message_filters
 import rospy
+import sys
 from sensor_msgs.msg import CompressedImage, CameraInfo, Joy
 
 
-rospy.init_node('steer_data_sync', anonymous=True)
+
 
 def callback(image, joy):
     print(image)
     print(joy)
+    print("here")
     # Save here
     return
 
-image_sub = message_filters.Subscriber('/output/image_raw/compressed', Image)
-joy_sub = message_filters.Subscriber('joy', Joy)
 
-ts = message_filters.TimeSynchronizer([image_sub, joy_sub], 10)
-ts.registerCallback(callback)
-rospy.spin()
+def main(args):
+    rospy.init_node('steer_data_sync', anonymous=True)
+    image_sub = message_filters.Subscriber('output/image_raw/compressed', CompressedImage)
+    joy_sub = message_filters.Subscriber('joy', Joy)
+
+    ts = message_filters.ApproximateTimeSynchronizer([image_sub, joy_sub], 10, 1)
+    ts.registerCallback(callback)
+    print("Spinning")
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
+if __name__ == '__main__':
+    main(sys.argv)
