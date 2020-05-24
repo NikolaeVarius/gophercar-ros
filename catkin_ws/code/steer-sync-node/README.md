ec61f48d4a8634ce1f6618d8af6193622505847f
jkim <joekim29@gmail.com>
2020-05-24 08:29:12 -0400

    updates

---
+++ catkin_ws/code/steer-sync-node/README.md
@@ -0,0 +1,11 @@
+# Notes
+
+Need to recompile for python3
+
+```console
+catkin_make --cmake-args \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+            -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m \
+            -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
+```
--- catkin_ws/code/steer-sync-node/main.py
+++ catkin_ws/code/steer-sync-node/main.py
@@ -5,17 +5,21 @@ import message_filters
 import rospy
 import sys
 from sensor_msgs.msg import CompressedImage, CameraInfo, Joy
+from cv_bridge import CvBridge, CvBridgeError
+import cv2 
+# from cv_bridge.boost.cv_bridge_boost import getCvType
 
-
-
+bridge = CvBridge()
 
 def callback(image, joy):
-    print(image)
-    print(joy)
-    print("here")
-    # Save here
-    return
-
+    img_timestamp = image.header.stamp.secs
+    joy_timestamp = joy.header.stamp.secs
+    try:
+        cv2_img = bridge.compressed_imgmsg_to_cv2(image, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+    else:
+        cv2.imwrite('camera_image.jpeg', cv2_img)
 
 def main(args):
     rospy.init_node('steer_data_sync', anonymous=True)
